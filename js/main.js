460ff34b2d9a92091783323c6ccf92c558a04c50
Landon Coffey <landoncof@thearrayschool.com>
2018-04-16 16:10:10 -0600

    Added Navigation Tabs, Request.js, and changed library name

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
-// Shorthand for $( document ).ready()
-$(() => {
-  console.log( "ready!" );
-  $('button').click(function () {
-    alert("jQuery alert!");
+/* global Requests */
+
+const libraryID = 156;
+const req = new Requests(libraryID);
+
+async function getLibraryName() {
+  let library = await req.getLibrary();
+  $('.jumbotron h1').text(library.name);
+}
+
+getLibraryName();
+
+async function testAPI(){
+  let book1 = await req.createBook({
+    title: "Maul Lockdown",
+    description: "Starwars Book about my favorite Sith ",
+    image_url: "https://vignette.wikia.nocookie.net/starwars/images/d/dc/MaulLockdownCover.jpg/revision/latest?cb=20130427064638"
   });
-});
+
+  console.log("After Book Creation");
+  console.log(book1);
+
+  console.log("Now we'll request all the books from the library");
+
+  let books = await req.getBooks();
+
+  console.log("After the get all the books request comes back");
+  console.log(books);
+}
